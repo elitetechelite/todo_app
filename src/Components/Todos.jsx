cf42d4ac015a4a693c6962e6f38851d8ebda7a4c
J Phone <jphon726@gmail.com>
2026-08-27 07:46:34 +0300

    Commit 8

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -154,7 +154,6 @@ export default function Todos() {
             // console.log("No Client Data ");
           }
         });
-
         console.log("Client DAT: ", client_data);
         clients_content.forEach((client) => {
           if (client.client_id == my_client_id) {
@@ -171,10 +170,7 @@ export default function Todos() {
           } else {
           }
         });
-        setClient_Data([
-          ...client_data,
-          (client_data = ".content.push(todo_obj)"),
-        ]);
+        setClient_Data([...client_data, client_data[0].content.push(todo_obj)]);
         setPersonal_Content({
           ...personal_content,
           client_data: client_data,
