93a217cd775f9dc4c06292ea6fcb195fa1c7b9d2
J Phone <jphon726@gmail.com>
2026-08-27 07:55:20 +0300

    Commit 8

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -155,7 +155,7 @@ export default function Todos() {
           }
         });
 
-        console.log("CLient Data: ", client_data);
+        console.log("CLient Data: ", clients_content);
 
         setClient_Data([
           ...client_data,
